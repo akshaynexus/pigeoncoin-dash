87c1850921ec26956ac833430494cbfa22360017
akshaynexus <akshaycm@hotmail.com>
2020-04-17 18:33:38 +0300

    Fix algo includes and related compile issues

--- src/crypto/sph_luffa.h
+++ src/crypto/sph_luffa.h
@@ -41,7 +41,7 @@ extern "C"{
 #endif
 
 #include <stddef.h>
-#include "sph_types.h"
+#include "algo/sph_types.h"
 
 /**
  * Output size (in bits) for Luffa-224.
--- src/crypto/sph_skein.h
+++ src/crypto/sph_skein.h
@@ -46,7 +46,7 @@ extern "C"{
 #endif
 
 #include <stddef.h>
-#include "sph_types.h"
+#include "algo/sph_types.h"
 
 #if SPH_64
 
