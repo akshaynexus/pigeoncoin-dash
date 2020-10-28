a159b3a4a4d3fd8a62d359ff19f67044689db250
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-10-28 22:02:05 +0300

    Call EraseObjectRequest as soon as an object is read from the stream (#3783)
    EraseObjectRequest should nat be postponed for later (no reason to do so) or skipped due to early returns (this is a bug).

--- src/llmq/quorums_instantsend.cpp
+++ src/llmq/quorums_instantsend.cpp
@@ -679,6 +679,13 @@ void CInstantSendManager::ProcessMessage(CNode* pfrom, const std::string& strCom
 
 void CInstantSendManager::ProcessMessageInstantSendLock(CNode* pfrom, const llmq::CInstantSendLock& islock, CConnman& connman)
 {
+    auto hash = ::SerializeHash(islock);
+
+    {
+        LOCK(cs_main);
+        EraseObjectRequest(pfrom->GetId(), CInv(MSG_ISLOCK, hash));
+    }
+
     bool ban = false;
     if (!PreVerifyInstantSendLock(pfrom->GetId(), islock, ban)) {
         if (ban) {
@@ -687,8 +694,6 @@ void CInstantSendManager::ProcessMessageInstantSendLock(CNode* pfrom, const llmq
         }
         return;
     }
-
-    auto hash = ::SerializeHash(islock);
 
     LOCK(cs);
     if (db.GetInstantSendLockByHash(hash) != nullptr) {
@@ -882,11 +887,6 @@ std::unordered_set<uint256> CInstantSendManager::ProcessPendingInstantSendLocks(
 
 void CInstantSendManager::ProcessInstantSendLock(NodeId from, const uint256& hash, const CInstantSendLock& islock)
 {
-    {
-        LOCK(cs_main);
-        EraseObjectRequest(from, CInv(MSG_ISLOCK, hash));
-    }
-
     CTransactionRef tx;
     uint256 hashBlock;
     const CBlockIndex* pindexMined = nullptr;
